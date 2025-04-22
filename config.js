548572b32b6e6a9e3f4d4ef575223c7612880a3e
Miguel Bullón <emebullon@MacBook-Pro-de-Miguel.local>
2025-04-22 18:09:43 +0200

    Configuración inicial para Netlify Functions

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    IDENTITY_URL: '/.netlify/functions/api/token',
+    API_BASE_URL: '/.netlify/functions/api',
+    APP_ID: 'livestats-miguelbullon',
+    APP_SECRET: 'Rk4wnoJqDI5ZyNFYSZS2cLMFLwSpL/RJyEDkVZEw9SXU='
+}; 
